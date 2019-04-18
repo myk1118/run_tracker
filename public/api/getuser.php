6ef8db2f428c1b2361e6791219d8ed8c8af395a6
Alice Le <lealice@gmail.com>
2019-04-17 19:35:29 -0700

    completed getuser endpoint

---
+++ public/api/getuser.php
@@ -0,0 +1,28 @@
+<?php
+require_once('functions.php');
+require_once('config.php');
+require_once('mysqlconnect.php');
+set_exception_handler('handleError');
+
+
+// if(empty($_SESSION['user_id'])){
+//     throw new Exception ('Missing user id');
+// }
+
+$user_id = 1;
+
+$query = "SELECT `first_name`, `last_name`, `email`, `password` FROM `users` WHERE `id` = $user_id ";
+
+$result = mysqli_query($conn, $query);
+
+if(!$result){
+    throw new Exception('invalid query: '. mysqli_error($conn));
+}
+
+if(mysqli_num_rows($result) === 0){
+    throw new Exception("no user");
+};
+
+$data = mysqli_fetch_assoc($result);
+
+print_r(json_encode($data));
