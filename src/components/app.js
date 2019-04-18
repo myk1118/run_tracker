3afb1d426206744f308d4d6110e64141a33b4d14
Davideuijinlee <Davideuijinlee@gmail.com>
2019-04-17 19:11:46 -0700

    creatde folders for nested components

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,16 @@
 
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import Runs from './runs';
+import Runs from './runs/runs';
 import TotalStats from './total_stats';
-import Chart from './chart';
+import Table from './table';
 import ApiTest from './api_test';
 import RunMap from './run_map/run_map';
-import RunResult from './run_map/run_results';
-import RunStats from './run_map/run_stats';
+// import RunResult from './run_map/run_results';
+// import RunStats from './run_map/run_stats';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './login';
-import Signup from './signup';
+import Login from './account/login/login';
+import Signup from './account/sign_up/sign_up';
 import Tutorial from './tutorial';
 
 const App = (props) => {
@@ -20,7 +20,7 @@ const App = (props) => {
           <Route exact path="/" component={Runs}/>
           <Route path="/runmap" component={RunMap}/>
           <Route path="/totalstats" component={TotalStats}/>
-          <Route path="/chart" component={Chart} />
+          <Route path="/table" component={Table} />
           {/* <Route path="/login" component={Login}/> */}
         </Switch>
       </div>
