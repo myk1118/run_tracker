37ef87765dbf28874d52a79553dea025dfb2c1d0
myk1118 <jaimekim1118@gmail.com>
2019-04-18 22:24:20 -0700

    Started on stopwatch

--- src/components/run_map/run_map.js
+++ src/components/run_map/run_map.js
@@ -1,18 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MapNav from '../nav_folder/map_nav';
+import Stopwatch from './stopwatch';
 
 class RunMap extends Component {
 
     render() {
         return (
             <div className="mapBody">
-                <MapNav/>
+                <MapNav />
                 <div className="h-60 d-inline-block mapContainer">
                     <div className="map"></div>
-                    <button className="bg-secondary startBtn">Start Button</button>
+                    <button className="btn btn-info">Start</button>
+                    <button className="btn btn-info">Pause</button>
+                    <button className="btn btn-info">Stop</button>
                 </div>
                 <div className="mapStatsContainer">
-                    <div className="statContainer">Time</div>
+                    <div className="statContainer">
+                        Time
+                        <Stopwatch />
+                    </div>
                     <div className="statContainer">Distance</div>
                     <div className="statContainer">Pace</div>
                     <div className="statContainer">Calories Burned</div>
