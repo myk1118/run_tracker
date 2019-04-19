4329bfcc96111db622e8f208ad4f602b2d31e3a3
Davideuijinlee <Davideuijinlee@gmail.com>
2019-04-19 14:40:48 -0700

    added working map axios

--- src/components/run_map/run_map.js
+++ src/components/run_map/run_map.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import MapNav from '../nav_folder/map_nav';
 import Stopwatch from './stopwatch';
 
 class RunMap extends Component {
+    state = {
+        distance: 100,
+        time: 100,
+        pace: 100,
+        calories: 100,
+        user_id: 2
+    }
+
     startWatch = () => {
         this.refs.child.start();
     }
@@ -11,7 +20,16 @@ class RunMap extends Component {
     }
     resetWatch = () => {
         this.refs.child.reset();
+        this.postCurrentRun();
     }
+    postCurrentRun = () =>{
+        const {distance, time, pace, calories, user_id} = this.state;
+        axios.get(`/api/addrun.php?user_id=${user_id}&distance=${distance}&time=${time}&pace=${pace}&calories=${calories}`).then((resp) => {
+            console.log('this is response:', resp);
+        });
+    }
+
+
     render() {
         return (
             <div className="mapBody">
