2cdfe7a6aa3053ca82df357cb61dfd633f177704
Alice Le <lealice@gmail.com>
2019-04-19 16:29:34 -0700

    added input/login form from redux, added manyyyyy folders and files confirmed input is working, need to fix css and buttons need to add styling to login page

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AccountRoutes from './account';
 import Runs from './runs/runs';
 import TotalStats from './total_stats';
 import Table from './total_stats/table';
@@ -10,14 +11,11 @@ import RunRoute from './run_map';
 // import RunResult from './run_map/run_results';
 // import RunStats from './run_map/run_stats';
 import Login from './account/login/login';
-import Signup from './account/sign_up/sign_up';
-import Tutorial from './tutorial';
-import MapNav from './nav_folder/map_nav'
+// import Signup from './account/sign_up/sign_up';
+// import Tutorial from './tutorial';
+import MapNav from './nav_folder/map_nav';
 
 const App = (props) => {
-
-  console.log(props)
-
   return (
     <Fragment>
       <Switch>
@@ -26,12 +24,11 @@ const App = (props) => {
         <Route path="/totalstats" component={TotalStats} />
         <Route path="/table" component={Table} />
         <Route path="/login" component={Login} />
+        <Route path="/account" component={AccountRoutes} />
       </Switch>
     </Fragment>
   )
 };
-
-
 
 {/* <ApiTest/> */ }
 export default App;
