ffcc5c5c533062bbdb67fe62c74abc3370a13d8e
myk1118 <jaimekim1118@gmail.com>
2019-04-21 23:43:42 -0700

    login page design fix

--- src/components/account/login/carousel.js
+++ src/components/account/login/carousel.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import image8 from './images/image8.jpeg';
-import image9 from './images/image9.jpeg';
-import image10 from './images/image10.jpeg';
+import image4 from './images/image4.jpeg';
+import image5 from './images/image5.jpeg';
+import image15 from './images/image15.jpeg';
 
 class Carousel extends Component {
     componentDidMount() {
@@ -19,13 +19,13 @@ class Carousel extends Component {
 
                     <div className="carousel-inner" role="listbox">
                         <div className="carousel-item active">
-                            <img className="image8 d-block" src={image8} alt="First slide" />
+                            <img className="image8 d-block" src={image4} alt="First slide" />
                         </div>
                         <div className="carousel-item">
-                            <img className="image9 d-block" src={image9} alt="Second slide" />
+                            <img className="image9 d-block" src={image5} alt="Second slide" />
                         </div>
                         <div className="carousel-item">
-                            <img className="image10 d-block" src={image10} alt="Third slide" />
+                            <img className="image10 d-block" src={image15} alt="Third slide" />
                         </div>
                     </div>
 
