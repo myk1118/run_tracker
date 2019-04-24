f3051989ba84fb6c131ee874923c2c4e502a98b5
jmp1234 <johnnypham944@yahoo.com>
2019-04-23 20:08:03 -0700

    used dummy data to create a polylinebetween locations

--- src/components/run_map/map.js
+++ src/components/run_map/map.js
@@ -9,6 +9,15 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) => {
       center={{ lat: lat, lng: lng }}
     >
       {props.isMarkerShown && <Marker position={{ lat: lat, lng: lng }} />}
+      <Polyline
+        path={[
+          {lat: 33.6349179, lng: -117.74050049999998},
+          {lat: 33.631, lng: -117.61},
+          {lat: 33.572, lng: -117.631},
+          {lat: 33.467, lng: -117.727}
+        ]}
+        strokeColor="#FF0000"
+      />
     </GoogleMap>
   )
 
