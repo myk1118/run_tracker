7d830f737caa6575b344a750efd10b2d49f1ab0c
Davideuijinlee <Davideuijinlee@gmail.com>
2019-04-26 17:01:40 -0700

    created dynamic run data

---
+++ public/api/get_last_runsession_results.php
@@ -0,0 +1,51 @@
+<?php
+require_once('functions.php');
+require_once('config.php');
+require_once('mysqlconnect.php');
+set_exception_handler('handleError');
+
+// $user_id = 1;
+// $run_id = $_GET['id'];
+$id = $_SESSION['user_data']['id'];
+
+$output = [
+  'success'=> false
+];
+
+
+$query = "SELECT r.`id`, r.`distance`, r.`time`, r.`calories`, r.`pace`, m.`mileage` AS `miles`, m.`time` AS `permiletime`
+FROM `run_stats` AS r
+JOIN `miles` as m
+ON r.`id` = m.`run_id`
+WHERE `user_id` = $id
+  ";
+
+
+
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    throw new Exception('invalid query: ' . mysqli_error($conn));
+}
+
+$output['success'] = true;
+
+
+while($row = mysqli_fetch_assoc($result)) {
+    $minutes = round((int)$row['time']/60 , 2);
+    $output[] = [
+        'id' => (int)$row['id'],
+        'time' => (int)$minutes,
+        'distance' => (int)$row['distance'],
+        'calories' => (int)$row['calories'],
+        'pace' => (int)$row['pace'],
+        'perMile'=> [
+            'currentMile' => (int)$row['miles'],
+            'perMileTime' => (int)$row['permiletime']
+        ]
+    ];
+  };
+
+print(json_encode($output));
+
+?>
