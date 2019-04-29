67b77dab8b0e8ca79060d765d9e79c86c091b603
jmp1234 <johnnypham944@yahoo.com>
2019-04-29 00:52:55 -0700

    made css changes to the personal bests section of the total stats page

--- src/components/total_stats/personal_bests.js
+++ src/components/total_stats/personal_bests.js
@@ -9,6 +9,7 @@ class PersonalBests extends Component {
       longestRun: null,
       fastestPace: null,
       lastRunDate: null,
+      averagePace: null
     }
   }
 
@@ -18,24 +19,86 @@ class PersonalBests extends Component {
 
   async getPersonalBests() {
     const bests = await axios.get('/api/personalbestquery.php');
-    const {longestRun, fastestpace, lastRunDate} = bests.data;
+    const {longestRun, fastestpace, lastRunDate, averagePace} = bests.data;
     console.log('bests: ',bests);
     this.setState({
       longestRun: longestRun,
       fastestPace: fastestpace,
       lastRunDate: lastRunDate,
+      averagePace
     })
   }
 
 
   render() {
-    const {longestRun, fastestPace, lastRunDate} = this.state;
+    const {longestRun, fastestPace, lastRunDate, averagePace} = this.state;
     return (
       <div className="personal-bests">
+      {/* <div className="personal-bests">
         <p><b>Fastest Pace: </b> <u>{fastestPace}</u></p>
         <p><b>Longest Run: </b><u>{longestRun} miles</u></p>
-        {/* <p>Most Calories Burned:</p> */}
         <p><b>Last Run: </b><u>{lastRunDate}</u></p>
+      </div> */}
+        <div className="row">
+          <div className="col-6">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Longest Run:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{longestRun} <span>miles</span></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-6">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Average Pace:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{averagePace}<span> minutes per mile</span></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Last Run:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{lastRunDate}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col-6">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Calories Burned:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>???<span> calories</span></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     )
   }
