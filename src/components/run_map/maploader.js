1a6a4b18e43bc320eed13591dbe9ed3070982d0b
jmp1234 <johnnypham944@yahoo.com>
2019-05-02 21:45:57 -0700

    renamed class to classname in maploader component

--- src/components/run_map/maploader.js
+++ src/components/run_map/maploader.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default props => {
   return (
-<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+<div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
   )
 }
