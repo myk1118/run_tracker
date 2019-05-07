1129ba63487185511f420c80be3ac171938c47fb
jmp1234 <johnnypham944@yahoo.com>
2019-05-06 20:26:40 -0700

    npm installed chartjs data labels gave charts proper titles and labels

--- src/components/total_stats/piechart.js
+++ src/components/total_stats/piechart.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
 import './total_stats.scss';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 export default props => {
   return (
@@ -9,11 +10,34 @@ export default props => {
       <Pie
         data={props.pieChartData}
         options = {{
+          tooltips: {
+            callbacks: {
+                label: function(tooltipItem, data) {
+                  console.log('hello')
+                  console.log('tooltip: ', tooltipItem);
+                  console.log('data: ', data)
+                  // let date = new Date(null);
+                  // date.setSeconds(tooltipItem.yLabel);
+                  return `${data.datasets[0].data[tooltipItem.index]} runs`;
+                }
+            }
+          },
+          plugins: {
+            datalabels: {
+               display: function(context) {
+                 return context.dataset.data[context.dataIndex] > 0
+               },
+               color: 'black',
+               font: {
+                 weight: 'bold',
+               }
+            }
+          },
           maintainAspectRatio: false,
           responsive: true,
           title: {
             display: true,
-            text: 'Distance Comparison',
+            text: 'Run Session Comparisons',
             fontSize: 25
           },
           legend: {
