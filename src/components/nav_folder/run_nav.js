45098bbbb71b906f358174b4b707c757843470e7
myk1118 <jaimekim1118@gmail.com>
2019-05-08 17:42:42 -0700

    Added a modal for tutorial

--- src/components/nav_folder/run_nav.js
+++ src/components/nav_folder/run_nav.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import Logout from '../account/logout';
 import './nav.scss';
 
 class RunHeader extends Component {
