5dec710a806e1af193966d47259bb32a8a5a3b17
jmp1234 <johnnypham944@yahoo.com>
2019-05-09 11:59:09 -0700

    added year to the date in the runs page

--- public/api/get_run_dates.php
+++ public/api/get_run_dates.php
@@ -25,7 +25,7 @@ while ($row = mysqli_fetch_assoc($result)) {
   $parent = $row['date'];
   $timestamp = strtotime($parent);
 
-  $date = date('n/j', $timestamp);
+  $date = date('n/j/y', $timestamp);
   $time = date('h:i a', $timestamp);
 
   $output['dates'][] = [
