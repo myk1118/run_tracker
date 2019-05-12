ad607c028ed8c7db39d8881e693e2d79b47a964c
jmp1234 <johnnypham944@yahoo.com>
2019-05-12 14:34:51 -0700

    added font awesome icons to total stats page

--- src/components/total_stats/personal_bests.js
+++ src/components/total_stats/personal_bests.js
@@ -26,7 +26,7 @@ export default props => {
           </div>
           <div className="col-3 col-sm-2">
             <NavLink to={`/results/${latestRunInfo.id}`}>
-              <p className="right-arrow text-center"><i><FontAwesomeIcon icon="chevron-right" color="rgba(107, 185, 240, 1)"/></i></p>
+              <p className="right-arrow text-center"><i><FontAwesomeIcon  icon="chevron-right" color="rgba(107, 185, 240, 1)"/></i></p>
             </NavLink>
           </div>
         </div>
@@ -34,7 +34,7 @@ export default props => {
 
         <div className="row bests-per-run">
           <p className="personal-bests-title">PERSONAL BESTS</p>
-          <div className="longestRun col-6">
+          <div className="col-6">
             <div>
               <p>Longest Run:</p>
               <div className="data-and-date-container">
@@ -43,7 +43,7 @@ export default props => {
               </div>
             </div>
           </div>
-          <div className="longestRun col-6">
+          <div className="col-6">
             <div>
               <p>Calories Burned:</p>
               <div className="data-and-date-container">
@@ -59,32 +59,32 @@ export default props => {
           <p className="personal-bests-title">ALL-TIME</p>
           <div className="all-time col-6">
             <div>
-              <p className="all-time-title">Average Pace:</p>
-              <div className="data-and-date-container">
+              <p className="all-time-title"><i><FontAwesomeIcon icon="stopwatch" color="rgba(130, 82, 0.8)"/></i> Average Pace:</p>
+              <div>
                 <p className="best">{averagePace}<span> {averagePace ? 'min/mile' : ''}</span></p>
               </div>
             </div>
           </div>
           <div className="all-time col-6">
             <div>
-              <p className="all-time-title">Total Calories:</p>
-              <div className="data-and-date-container">
+              <p className="all-time-title"><i><FontAwesomeIcon icon="fire" color="rgba(226,88,34,0.8)"/></i> Total Calories:</p>
+              <div>
                 <p className="best">{totalCalories}<span> {mostCalories ? 'calories' : ''}</span></p>
               </div>
             </div>
           </div>
           <div className="all-time col-6">
             <div>
-              <p className="all-time-title">Total Miles</p>
-              <div className="data-and-date-container">
+              <p className="all-time-title"><i><FontAwesomeIcon icon="road" color="grey"/></i> Total Miles:</p>
+              <div>
                 <p className="best">{totalDistance}<span> {totalDistance ? 'miles' : ''}</span></p>
               </div>
             </div>
           </div>
           <div className="all-time col-6">
             <div>
-              <p className="all-time-title">Total Minutes Ran</p>
-              <div className="data-and-date-container">
+              <p className="all-time-title"><i><FontAwesomeIcon icon="clock" color="rgba(107, 185, 240, 1)"/></i> Total Minutes:</p>
+              <div>
                 <p className="best">{totalTimeInMinutes}<span> {totalTimeInMinutes ? 'minutes' : ''}</span></p>
               </div>
             </div>
