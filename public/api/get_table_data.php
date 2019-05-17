09c4ec4479e4ebd9559f60cdeb7fe9b6f649cc31
jmp1234 <johnnypham944@yahoo.com>
2019-05-16 18:04:18 -0700

    fixed date filters in get_table_data.php so that activity log properly displays on safari browsers

--- public/api/get_table_data.php
+++ public/api/get_table_data.php
@@ -28,7 +28,7 @@ while($row = mysqli_fetch_assoc($result)) {
   $date= new DateTime($row['date']);
 
   $output['tableItems'][] = [
-  'date' => ltrim($date->format('m-d-Y'), '0'),
+  'date' => ltrim($date->format('m/d/Y'), '0'),
   'distance' => (float)$row['distance'],
   'time' => gmdate('H:i:s', $row['time']),
   'pace' => (int)$row['pace'],
