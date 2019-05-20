3b10432f746908566e8f0508ec764a684b419336
jmp1234 <johnnypham944@yahoo.com>
2019-05-20 11:15:09 -0700

    padding in results and total stats now the same

--- src/components/run_results/results_display.js
+++ src/components/run_results/results_display.js
@@ -10,27 +10,27 @@ export default props => {
   return (
     <Fragment>
       <div className="row">
-        <div className="col-6">
+        <div className="col-6 results-left-column">
           <div className="results-display-container">
-            <p className="runtitle"><i><FontAwesomeIcon icon="clock" color="rgba(107, 185, 240, 1)"/></i> Duration (min:sec)</p>
+            <p className="runtitle"><i><FontAwesomeIcon icon="clock" color="rgba(107, 185, 240, 1)"/></i> Duration <span className="results-display-units">(min:sec)</span></p>
             <p>{minutesSecondsRan}</p>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 results-right-column">
           <div className="results-display-container">
-            <p className="runtitle"><i><FontAwesomeIcon icon="stopwatch" color="rgba(130, 82, 0.8)"/></i> Avg Pace (min/mile)</p>
+            <p className="runtitle"><i><FontAwesomeIcon icon="stopwatch" color="rgba(130, 82, 0.8)"/></i> Avg Pace <span className="results-display-units">(min/mile)</span></p>
             <p>{paceWithNaNCheck}</p>
           </div>
         </div>
       </div>
       <div className="row">
-        <div className="col-6">
+        <div className="col-6 results-left-column">
           <div className="results-display-container">
             <p className="runtitle"><i><FontAwesomeIcon icon="road" color="grey"/></i> Distance</p>
             <p>{distance} miles</p>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 results-right-column">
           <div className="results-display-container">
             <p className="runtitle"><i><FontAwesomeIcon icon="fire" color="rgba(226,88,34,0.8)"/></i> Calories</p>
             <p>{calories}</p>
