1b0fbe2a74300d592308acd5fbee2522ab97c33d
jmp1234 <johnnypham944@yahoo.com>
2019-06-02 16:54:45 +0200

    created error messages in case location cant be found

--- src/components/run_map/maploader.js
+++ src/components/run_map/maploader.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default props => {
-  return (
-<div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-  )
+    return(
+        props.errorMessage ?
+            <div className="errorMessage">
+                <div className="text-center mb-3">
+                    <FontAwesomeIcon icon="exclamation-circle" color="grey" size="4x" />
+                </div>
+                {props.errorMessage}
+            </div> :
+
+            <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
 }
